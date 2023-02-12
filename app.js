9aaa4bad70d0b4d5d18cb32d4a4b5a7939e2d6cb
myduyen47 <duyenb1909892@student.ctu.edu.vn>
2023-02-12 23:59:29 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require("cors");
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
 
 app.use(cors());
 app.use(express.json());
@@ -11,5 +13,16 @@ app.get("/", (req,res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((req, res, next) =>{
+    return res.status(error.statusCode || 500).json ({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 module.exports = app;
 
